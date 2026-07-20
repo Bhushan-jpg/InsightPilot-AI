6781c2fd96c6a9f10b200f5a50d6eed30c0de6f2
morebhushan0118-a11y <morebhushan0118@gmail.com>
2026-07-20 13:15:38 +0530

    Connect Render PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/ai_bi_platform"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
